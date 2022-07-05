b723f289cc1a4364facbcbe9d2963eb83503b4c0
Creola Tudorache <tudorachecreola@gmail.com>
2022-07-05 11:47:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+//MODEL OBIECT
+
+// const person = {
+//     name: 'nume',
+//     address: {
+//       country: 'tara',
+//       city: 'oras',
+//       street: 'nume strada',
+//       number: 'numar strada'
+//     },
+//     age: 'varsta',
+//     sayHello: function() {
+//       // 'faceti un template literals nu uitati folositi backtick simbolul de sub esc cautati unde este exact pe tastatura ca sa puneti valori'
+//       console.log(`exemplu ${this.name}`)
+//     }
+//   }
+// console.log(person.name);
+// person.sayHello();
+
+// creati persoane noi si adaugati in array ca in live. liberi sa faceti orice proprietate vreti. Puteti sa faceti si masini de exemplu marca model an nr cai etc
+
+// const car = {
+//     make: 'VW',
+//     model: 'Golf'
+//   }
+  //Model mai sus
+
+//   const walkingDead = {
+//     actor: {
+//         name: `Andrew Lincoln`
+//     },
+//     character: {
+//         nameCharacter: `Rick Grimes`,
+//         firstYear: 2010,
+//         lastYear: 2021,
+//         episodes: 122
+//     },
+//     yearsCharacter: function() {
+//         console.log( this.character.lastYear - this.character.sfirstYear) 
+//     }
+// }
+ 
+// console.log(walkingDead.actor.name);
+// walkingDead.yearsCharacter()
+
+// let fruit = {
+//     fruitName: "Apple",
+//     taste: "Sweet",
+//     weight: 100
+//   }
+  
+  
+//   console.log(`The ${fruitName} is ${taste} and it weights ${weight} grams`);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"] You can of course add more elements to the array
+
+// Given this array
+
+// Create a HTML list that contains the elements of the fruits array
+// Banana
+// Orange
+// Apple
+// Mango
+// Clues! Declare a variable that will be the ul iterate the fruits array and create the li elements *use appendChild method to add the li elements to the ul For the number in front of every fruit use the "i" parameter that represents the position of the iterator.
+
+// Apply styles to the list. In the design you will find only three fruits but there can be unlimited depending on the array length and content
+  
+let header= document.getElementsByClassName('header');
+let firstDiv = document.querySelector('div');
+firstDiv.style = 'width: 500px; height: 100vh; margin: 0, auto; border: 10px solid blue; text-align: center; padding: 10px 10px;  box-shadow: 1px 1px #888888;';
+let para = document.querySelector('p');
+para.style = 'font-weight: 700; font-size: 3rem; background-color: green;';
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let createElements () {
+//     let list = document.createElement("ul");
+//     document.firstDiv.appendChild(list)
+// }
+// console.log(createElements)
+
+//     
+//  
+//  
+//     holderContainer.appendChild(container);
+// }
+// 
+// let creatList () => {
+//     let list = document.createElement("ul");
+//     let listOfFruits = document.getElementsByClassName("header");
+//     header.appendChild('li');
+// }
+// listFruit.map((fruit) => {
+//     
+// })
+// let theList = Array.from(document.querySelectorAll('li'));
+// theList.map(fruit, i) => {
+//     theList.innerHTML
+// }
+
+ // header.createElement = 'h1'
+    // header.textContent = 'List of fruits';
+    // header.innerHTML = '<h1>List of fruits</h1>';
+// header.innerHTML = '<h1>List of fruits</h1>';
+// let headerTitle= document.innerHTML('headerTitle');
+// let newList = document.createElement(`list`) ;
+
+// newList.textContent = 
+
+// for(let i=0; i < array.length; i++) {
+//     console.log(array[i])
+// }
+
+// let function = () => {}
+
+// fruits.map((fruit) => {
+//     console.log(fruit)
+// })
